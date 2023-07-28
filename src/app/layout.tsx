2675460dcca55d117b5cc3dfe430ba1437bfbfde
unknown <VasaPofficial@gmail.com>
2023-07-28 17:14:24 +0200

    build: initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,27 @@
+import './global.css';
+
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  generator: 'Next.js',
+  referrer: 'origin-when-cross-origin',
+  keywords: ['project keyworks'],
+  title: 'Project',
+  creator: 'Author',
+  description: 'Give description',
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
