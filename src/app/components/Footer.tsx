7cc31a6aef7402317775b1375c8fe0861767281d
unknown <VasaPofficial@gmail.com>
2023-07-31 18:34:42 +0200

    feat: add footbar icons and license

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,7 +1,13 @@
+import Image from 'next/image';
+
+import EmailIcon from '@/public/assets/Email.png';
+import GoogleMapIcon from '@/public/assets/Google Map.png';
+import PhoneIcon from '@/public/assets/phone icon.png';
+
 export default function Footer() {
   return (
-    <div>
-      <footer className="flex h-[750px] w-full items-center  justify-evenly bg-footer ">
+    <footer className="flex h-[700px] w-full flex-col items-center justify-evenly bg-footer ">
+      <div className="flex ">
         <iframe
           title="googleMap"
           id="gmap_canvas"
@@ -9,25 +15,37 @@ export default function Footer() {
           height="400px"
           src="https://maps.google.com/maps?width=700&amp;height=350&amp;hl=en&amp;q=Belgrade, Kneza Milosa 17&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
         />
-        <div className="flex flex-col italic text-white">
+        <div className="ml-32 mt-20 flex flex-col italic text-white">
           <p className="text-4xl font-bold tracking-wide underline">
             Contact info
           </p>
           <br />
           <br />
-          <div className="text-3xl font-semibold">
-            <div className="flex items-center">
+          <div className="flex flex-col gap-5 text-3xl font-semibold">
+            <div className="flex gap-3">
+              <Image src={PhoneIcon} alt="phone icon" width={40} height={30} />
               <p>06523213122</p>
             </div>
-            <div className="flex items-center">
+            <div className="flex gap-3">
+              <Image
+                src={GoogleMapIcon}
+                alt="phone icon"
+                width={40}
+                height={30}
+              />
               <p>Beograd, Kneza Milosa 17</p>
             </div>
-            <div className="flex items-center">
+            <div className="flex gap-3">
+              <Image src={EmailIcon} alt="phone icon" width={40} height={30} />
               <p>teoprof@gmail.com</p>
             </div>
           </div>
         </div>
-      </footer>
-    </div>
+      </div>
+      <div className="mt-5 w-full max-w-[85%] border-t border-gray-500" />
+      <p className="text-white">
+        Copyright © 2023 Teoprof. Sva prava su zadržana.
+      </p>
+    </footer>
   );
 }
