c68a53337ba102470f2a559dacff8740d300a8b7
VasaOfficial <vasapofficial@gmail.com>
2023-08-03 19:54:45 +0200

    fix: fix the bug with zoomed image not closing

--- src/components/ui/zoomedImage.tsx
+++ src/components/ui/zoomedImage.tsx
@@ -8,9 +8,13 @@ const ZoomedImage = (props: {
   alt: string;
 }) => {
   return (
-    <div className="fixed left-0 top-0 z-50 flex h-screen w-screen items-center justify-center bg-black bg-opacity-[70]">
+    // eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions
+    <div
+      onClick={props.onClose}
+      // eslint-disable-next-line tailwindcss/migration-from-tailwind-2
+      className="fixed left-0 top-0 z-50 flex h-screen w-screen items-center justify-center bg-black bg-opacity-70"
+    >
       <Image
-        onClick={props.onClose}
         src={props.src}
         alt={props.alt}
         className="rounded-2xl"
